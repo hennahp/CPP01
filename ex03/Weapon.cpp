8d2b6e1d32a724ae458cfadc768a2b62cea83981
hennahp <hennaparveen77@gmail.com>
2025-10-29 19:29:34 +0400

    ex03 done

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -10,3 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+
+Weapon::Weapon(std::string type) : type(type) {}
+
+Weapon::~Weapon() {}
+
+const std::string& Weapon::getType() const
+{
+    return type;
+}
+
+void Weapon::setType(const std::string& newType)
+{
+    type = newType;
+}
+
